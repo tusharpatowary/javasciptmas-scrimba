2e87ab3f8658b5774341f8f3b7a816cf4038dadd
Md. Tushar Ahmed Patowary <60696339+tusharpatowary@users.noreply.github.com>
2024-01-05 23:22:04 +0600

    javascript code added for challenge day seven

---
+++ daySeven/index.js
@@ -0,0 +1,10 @@
+const wishlist = [
+  "Macbook Air M2"
+  // TODO: Add more items here
+];
+
+/** Challenge: 
+  - Iterate over the wishlist array.
+  - Dynamically render your wishlist from the array.
+  - Style the wishlist with CSS.
+**/
