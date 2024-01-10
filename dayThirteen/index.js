f0704b048296364a4ea282e66e28947708ed8cdd
Md. Tushar Ahmed Patowary <60696339+tusharpatowary@users.noreply.github.com>
2024-01-10 23:00:48 +0600

    Add files via upload

---
+++ dayThirteen/index.js
@@ -0,0 +1,17 @@
+/**
+Task:
+- Write the code to help a user choose the perfect Christmas dinner idea
+  based on the number of people attending.
+- Include a checkbox for the user to specify the meal 
+  should be vegetarian-friendly.
+
+Dinner suggestions (or choose your own!):
+Vegetarian: Winter Squash Risotto
+4 people or less: Ham
+5+ people: Turkey
+
+Stretch goals:
+- Add more dietary options.
+- Show recipes when the meal has been selected.
+ */
+
